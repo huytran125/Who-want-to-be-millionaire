f7aafc7e98291a5e14f5e1b3a6e2a1fcf534ea0b
unknown <huy.thekopVN@gmail.com>
2020-05-04 14:33:37 +0700

    first commit

---
+++ AiLaTrieuPhu/Form1.Designer.cs
@@ -0,0 +1,50 @@
+﻿namespace AiLaTrieuPhu
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.SuspendLayout();
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackgroundImage = global::AiLaTrieuPhu.Properties.Resources._61c4e6c9a1cafc37f620fa3e147245f4__1_;
+			this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.ClientSize = new System.Drawing.Size(1371, 725);
+			this.DoubleBuffered = true;
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+	}
+}
+
---
+++ README.md
@@ -0,0 +1,3 @@
+# Game Who want to be a millionaire
+# Build with C# and Entity Framework 
+# Use FPT voice API to add voice to game
